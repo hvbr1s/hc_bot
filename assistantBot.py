291ed1be9b31f9c69dc70695b7e3e50b843b5825
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-06-04 01:00:38 +0700

    Update assistantBot.py

--- assistantBot.py
+++ assistantBot.py
@@ -65,9 +65,9 @@ llm=ChatOpenAI(
 
 pinecone.init(api_key=os.environ['PINECONE_API_KEY'], enviroment=os.environ['PINECONE_ENVIRONMENT'])
 pinecone.whoami()
-index_name = 'hc'
+#index_name = 'hc'
 #index_name = 'hctest'
-#index_name = 'academyzd'
+index_name = 'academyzd'
 index = pinecone.Index(index_name)
 
 embed_model = "text-embedding-ada-002"
